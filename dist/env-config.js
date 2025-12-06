c3bf56b6252612996250eb70522f246b56997fb8
Makaveli1000 <macc10dime@gmail.com>
2025-12-05 22:31:17 -0600

    feat: Updated built assets in dist/ including corrected index.html paths

--- dist/env-config.js
+++ dist/env-config.js
@@ -1,11 +1,11 @@
 window.NETLIFY_FIREBASE_CONFIG = {
-  "apiKey": "AIzaSyCVDpWCX1Ntu7Obepe9fQRsDZQCVHxDI4I",
-  "authDomain": "sntlmoexclusivesportsgrid.firebaseapp.com",
-  "projectId": "sntlmoexclusivesportsgrid",
-  "storageBucket": "sntlmoexclusivesportsgrid.firebasestorage.app",
-  "messagingSenderId": "735791748207",
-  "appId": "1:735791748207:web:74fd6412684db238b6e99a"
+  "apiKey": "",
+  "authDomain": ".firebaseapp.com",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": ""
 };
-window.__app_id = "1:735791748207:web:74fd6412684db238b6e99a";
-window.__project_id = "sntlmoexclusivesportsgrid";
-window.GEMINI_API_KEY = "AIzaSyCVDpWCX1Ntu7Obepe9fQRsDZQCVHxDI4I";
+window.__app_id = "";
+window.__project_id = "";
+window.GEMINI_API_KEY = "";
