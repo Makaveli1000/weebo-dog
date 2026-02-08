509540690e1300d68195e883040db3e172b24338
Makaveli1000 <macc10dime@gmail.com>
2026-02-08 10:48:38 -0600

    Correct esbuild entry point to src/index.js and add ES Module output for browser

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,14 @@
-// esbuild.config.js
-const esbuild = require('esbuild');
+    // esbuild.config.js
+    const esbuild = require('esbuild');
 
-esbuild.build({
-  entryPoints: ['src/index.js'], // <--- CHANGE THIS to src/index.js
-  outfile: 'dist/bundle.js',
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  target: ['chrome60', 'firefox60', 'safari11', 'edge20'],
-}).catch(() => process.exit(1));
+    esbuild.build({
+      entryPoints: ['src/index.js'], // Assuming src/index.js is your main entry now
+      outfile: 'dist/bundle.js',
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      target: ['chrome60', 'firefox60', 'safari11', 'edge20'],
+      format: 'esm',      // <--- NEW: Output ES Module format
+      platform: 'browser' // <--- NEW: Target browser environment
+    }).catch(() => process.exit(1));
+   
