8bcc826e4531b017185c3df34ad7729bafdf8f26
Makaveli1000 <macc10dime@gmail.com>
2026-05-25 23:14:01 -0500

    Use app.js as the only build entry

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,12 @@
-// esbuild.config.js
 const esbuild = require('esbuild');
 
 esbuild.build({
-  entryPoints: ['src/index.js'], // This is now the true entry point that imports app.js
+  entryPoints: ['src/app.js'],
   outfile: 'dist/bundle.js',
   bundle: true,
   minify: true,
   sourcemap: true,
   target: ['chrome60', 'firefox60', 'safari11', 'edge20'],
-  format: 'esm',      // CRITICAL ADDITION: Output ES Module format
-  platform: 'browser' // CRITICAL ADDITION: Target browser environment
+  format: 'esm',
+  platform: 'browser'
 }).catch(() => process.exit(1));
