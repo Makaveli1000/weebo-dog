d2e132da73cdc0a2f65111911a505258bbeb1049
Makaveli1000 <macc10dime@gmail.com>
2026-06-26 23:03:28 -0500

    build: force production override compile

---
+++ dist/env-config.js
@@ -0,0 +1,9 @@
+window.NETLIFY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDbt0ITM9G4LOZTlXuAGGvuO80uazFpZSs",
+  authDomain: "sntlmoexclusivesportsgrid.firebaseapp.com",
+  projectId: "sntlmoexclusivesportsgrid",
+  storageBucket: "sntlmoexclusivesportsgrid.firebasestorage.app",
+  messagingSenderId: "735791748207",
+  appId: "1:735791748207:web:74fd6412684db238b6e99a",
+  measurementId: "G-T8RJPDPL4G"
+};
